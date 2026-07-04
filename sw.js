d959ad1ef75643f715e7ed593428a92ab72c7ef5
ameroglu-spec <ameroglu@gmail.com>
2026-07-04 22:04:01 +0300

    fix: improve reports layout responsiveness

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='momentum-hub-v5-1-3';
+const CACHE='momentum-hub-v5-1-4';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest'])))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const clone=r.clone();caches.open(CACHE).then(c=>c.put(e.request,clone)).catch(()=>{});return r}).catch(()=>caches.match(e.request))) });
